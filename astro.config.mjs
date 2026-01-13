f92e66c3ece2be6a9c56e0e1bc3c97f55c544ade
Thomas (Aeshus) <git@aeshus.com>
2026-01-12 20:20:35 -0600

    Format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import mdx from '@astrojs/mdx';
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+    site: "https://ritsec.club",
+    integrations: [mdx()],
 });
