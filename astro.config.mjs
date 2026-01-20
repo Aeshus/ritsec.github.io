cf29d84172d169e1af90f07ef583a0ff4e153e0c
Thomas (Aeshus) <git@aeshus.com>
2026-01-20 09:36:24 -0600

    Add React + Format

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,10 @@ import mdx from "@astrojs/mdx";
 
 import expressiveCode from "astro-expressive-code";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
     site: "https://ritsec.club",
-    integrations: [expressiveCode(), mdx()],
+    integrations: [expressiveCode(), mdx(), react()],
 });
