7c0b1e2fda448846b9d7058ab79485f931082fa9
Thomas (Aeshus) <git@aeshus.com>
2026-02-04 17:12:17 -0600

    Fix base URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,5 +10,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
     site: "https://aeshus.github.io",
+    base: "/ritsec.github.io",
     integrations: [expressiveCode(), mdx(), react()],
 });
