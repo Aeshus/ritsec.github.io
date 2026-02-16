91828110abb5f1341aca428bc1b57c18d7e45806
Thomas (Aeshus) <git@aeshus.com>
2026-02-16 15:29:19 -0500

    Draft

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,11 @@ import expressiveCode from "astro-expressive-code";
 
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
     site: "https://aeshus.github.io",
-    base: "/ritsec.github.io",
-    integrations: [expressiveCode(), mdx(), react()],
+    //base: "/ritsec.github.io",
+    integrations: [expressiveCode(), mdx(), react(), sitemap()],
 });
